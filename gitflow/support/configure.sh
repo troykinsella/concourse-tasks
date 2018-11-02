ccbf3f639e0fffb0cccfa97db1938c1b7c6ba48f
Troy Kinsella <troy.kinsella@startmail.com>
2018-11-01 17:49:32 -0700

    Betterness

--- gitflow/support/configure.sh
+++ gitflow/support/configure.sh
@@ -1,6 +1,17 @@
 
+gf_set() {
+  local key=$1
+  local value=$2
+  git flow config set --local $key $value
+}
+
 git flow init -fd
 
-if [ -n "$VERSION_PREFIX" ]; then
-  egrep -q 'versiontag =\s?$' ~/.git/config && sed -i "s/versiontag =/versiontag = $VERSION_PREFIX/" ~/.git/config
-fi
+gf_set master     ${GIT_FLOW_MASTER_BRANCH:-master}
+gf_set develop    ${GIT_FLOW_DEVELOP_BRANCH:-develop}
+gf_set feature    ${GIT_FLOW_FEATURE_PREFIX:-feature/}
+gf_set bugfix     ${GIT_FLOW_BUGFIX_PREFIX:-bugfix/}
+gf_set release    ${GIT_FLOW_RELEASE_PREFIX:-release/}
+gf_set hotfix     ${GIT_FLOW_HOTFIX_PREFIX:-hotfix/}
+gf_set support    ${GIT_FLOW_SUPPORT_PREFIX:-support/}
+gf_set versiontag ${VERSION_PREFIX:-""}
