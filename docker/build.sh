264fedbd873b5d49a4a34d24b3904b4743801040
Troy Kinsella <troy.kinsella@startmail.com>
2018-12-10 14:23:56 -0800

    Add Docker init support. Add Docker build task.

---
+++ docker/build.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -e
+
+ROOT=$PWD/$1
+OUT=$PWD/$2
+DOCKERFILE=${DOCKERFILE:-Dockerfile}
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
+source $DIR/support/init.sh
+
+target_option() {
+  if [ -n "$TARGET" ]; then
+    echo "--target $TARGET"
+  fi
+}
+
+start_docker
+
+if [ -n "$REGISTRY" ]; then
+  test -z "$USERNAME" && { echo "USERNAME required when REGISTRY supplied" >&2; exit 1; }
+  test -z "$PASSWORD" && { echo "PASSWORD required when REGISTRY supplied" >&2; exit 1; }
+
+  docker_log_in "$USERNAME" "$PASSWORD" "$REGISTRY"
+fi
+
+expanded_build_args=()
+build_arg_keys=($(echo "$BUILD_ARGS" | jq -r 'keys | join(" ")'))
+if [ "${#build_arg_keys[@]}" -gt 0 ]; then
+  for key in "${build_arg_keys[@]}"; do
+    value=$(echo "$BUILD_ARGS" | jq -r --arg "k" "$key" '.[$k]')
+    expanded_build_args+=("--build-arg")
+    expanded_build_args+=("${key}=${value}")
+  done
+fi
+
+expanded_labels=()
+label_keys=($(echo "$LABELS" | jq -r 'keys | join(" ")'))
+if [ "${#label_keys[@]}" -gt 0 ]; then
+  for key in "${label_keys[@]}"; do
+    value=$(echo "$labels" | jq -r --arg "k" "$key" '.[$k]')
+    expanded_labels+=("--label")
+    expanded_labels+=("${key}=${value}")
+  done
+fi
+
+docker build ${target_option} ${expanded_build_args[@]} ${expanded_labels[@]} -f $DOCKERFILE $ROOT
+
+if [ "$SAVE" = "true" ]; then
+  IMAGE_ID=$(docker images | awk '{print $1}' | awk 'NR==2')
+
+  COMPRESS_FILTER=cat
+  IMAGE_EXT=tar
+  if [ "$COMPRESS_SAVE" = "true" ]; then
+    COMPRESS_FILTER=gzip
+    IMAGE_EXT=tar.gz
+  fi
+
+  IMAGE_FILE="$OUT/image.$IMAGE_EXT"
+  echo "Saving image to $IMAGE_FILE"
+
+  docker save $IMAGE_ID | $COMPRESS_FILTER > $IMAGE_FILE
+fi
---
+++ docker/support/init.sh
@@ -0,0 +1,117 @@
+LOG_FILE=${LOG_FILE:-/tmp/docker.log}
+SKIP_PRIVILEGED=${SKIP_PRIVILEGED:-false}
+STARTUP_TIMEOUT=${STARTUP_TIMEOUT:-120}
+MAX_CONCURRENT_DOWNLOADS=${MAX_CONCURRENT_DOWNLOADS:-3}
+MAX_CONCURRENT_UPLOADS=${MAX_CONCURRENT_UPLOADS:-3}
+PID_FILE=/tmp/docker.pid
+
+sanitize_cgroups() {
+  mkdir -p /sys/fs/cgroup
+  mountpoint -q /sys/fs/cgroup || \
+    mount -t tmpfs -o uid=0,gid=0,mode=0755 cgroup /sys/fs/cgroup
+
+  mount -o remount,rw /sys/fs/cgroup
+
+  sed -e 1d /proc/cgroups | while read sys hierarchy num enabled; do
+    if [ "$enabled" != "1" ]; then
+      # subsystem disabled; skip
+      continue
+    fi
+
+    grouping="$(cat /proc/self/cgroup | cut -d: -f2 | grep "\\<$sys\\>")" || true
+    if [ -z "$grouping" ]; then
+      # subsystem not mounted anywhere; mount it on its own
+      grouping="$sys"
+    fi
+
+    mountpoint="/sys/fs/cgroup/$grouping"
+
+    mkdir -p "$mountpoint"
+
+    # clear out existing mount to make sure new one is read-write
+    if mountpoint -q "$mountpoint"; then
+      umount "$mountpoint"
+    fi
+
+    mount -n -t cgroup -o "$grouping" cgroup "$mountpoint"
+
+    if [ "$grouping" != "$sys" ]; then
+      if [ -L "/sys/fs/cgroup/$sys" ]; then
+        rm "/sys/fs/cgroup/$sys"
+      fi
+
+      ln -s "$mountpoint" "/sys/fs/cgroup/$sys"
+    fi
+  done
+
+  if ! test -e /sys/fs/cgroup/systemd ; then
+    mkdir /sys/fs/cgroup/systemd
+    mount -t cgroup -o none,name=systemd none /sys/fs/cgroup/systemd
+  fi
+}
+
+start_docker() {
+  mkdir -p /var/log
+  mkdir -p /var/run
+
+  if [ "$SKIP_PRIVILEGED" = "false" ]; then
+    sanitize_cgroups
+
+    # check for /proc/sys being mounted readonly, as systemd does
+    if grep '/proc/sys\s\+\w\+\s\+ro,' /proc/mounts >/dev/null; then
+      mount -o remount,rw /proc/sys
+    fi
+  fi
+
+  local mtu=$(cat /sys/class/net/$(ip route get 8.8.8.8|awk '{ print $5 }')/mtu)
+  local server_args="--mtu ${mtu}"
+  local registry=""
+
+  server_args="${server_args} --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS --max-concurrent-uploads=$MAX_CONCURRENT_UPLOADS"
+
+  try_start() {
+    dockerd ${server_args} >$LOG_FILE 2>&1 &
+    echo $! > "$PID_FILE"
+
+    sleep 1
+
+    echo waiting for docker to come up...
+    until docker info >/dev/null 2>&1; do
+      sleep 1
+      if ! kill -0 "$(cat $PID_FILE)" 2>/dev/null; then
+        return 1
+      fi
+    done
+  }
+
+  export server_args LOG_FILE
+  declare -fx try_start
+  trap stop_docker EXIT
+
+  if ! timeout ${STARTUP_TIMEOUT} /bin/bash -ce 'while true; do try_start && break; done'; then
+    echo Docker failed to start within ${STARTUP_TIMEOUT} seconds.
+    cat $LOG_FILE
+    return 1
+  fi
+}
+
+stop_docker() {
+  if [ ! -f $PID_FILE ]; then
+    return 0
+  fi
+
+  local pid=$(cat $PID_FILE)
+  if [ -z "$pid" ]; then
+    return 0
+  fi
+
+  kill -TERM $pid
+}
+
+docker_log_in() {
+  local username="$1"
+  local password="$2"
+  local registry="$3"
+
+  docker login -u "${username}" -p "${password}" ${registry}
+}
