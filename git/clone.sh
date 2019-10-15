b21083a58104cfe2a7aac57a4acb5459f098b270
Troy Kinsella <troykinsella@users.noreply.github.com>
2019-10-15 14:59:04 -0700

    Add dynamic checkout feature. Document task params. (#1)

--- git/clone.sh
+++ git/clone.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-OUT=$PWD/$1
+BRANCH_FILE_SOURCE="$PWD/$1"
+OUT="$PWD/$2"
 
 test -z "$REPO" && { echo "REPO parameter must be supplied" >&2; exit 1; }
 
@@ -13,16 +14,21 @@ $DIR/support/configure.sh
 cd $OUT
 
 echo "Cloning $REPO"
-git clone $REPO .
+git clone "$REPO" .
 git_track_remotes
 
 if [ "$GIT_FLOW" = "true" ]; then
   $DIR/../gitflow/support/configure.sh
 fi
 
-if [ -n "$BRANCH" ]; then
+if [ -n "$BRANCH" ] || [ -n "$BRANCH_FILE" ]; then
+  if [ -n "$BRANCH_FILE" ]; then
+    BRANCH=$(cat "$BRANCH_FILE_SOURCE/$BRANCH_FILE")
+  fi
+
   echo "Checking out branch: $BRANCH"
   git checkout $BRANCH
+
 elif [ "$GIT_FLOW_CHECKOUT_RELEASE" = "true" ]; then
   echo "Checking out git flow release branch"
   source $DIR/../gitflow/support/functions.sh
