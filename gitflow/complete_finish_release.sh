7ac238a49647187ad8a6ce058f97db03aa121473
Troy Kinsella <troy.kinsella@startmail.com>
2019-11-12 12:45:16 -0800

    Support git flow custom release tag message.

--- gitflow/complete_finish_release.sh
+++ gitflow/complete_finish_release.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-set -ex
+set -eu
 
-SOURCE_IN=$PWD/$1
+SOURCE_IN="$PWD/$1"
+MESSAGE_DIR="$PWD/$2"
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
 source $DIR/../git/support/functions.sh
@@ -11,12 +12,18 @@ $DIR/../git/support/configure.sh
 
 cd $SOURCE_IN
 
-VERSION=$(gitflow_require_release_version)
+message_opt() {
+  if [ -n "$MESSAGE" ]; then
+    echo "-m '$MESSAGE'"
+  elif [ -n "$MESSAGE_FILE" ]; then
+    echo "-m '$(cat "$MESSAGE_DIR/$MESSAGE_FILE")'"
+  fi
+}
 
 if [ "$DRY_RUN" = "true" ]; then
   echo "Dry run: Skipping release finish"
 else
   export GIT_MERGE_AUTOEDIT=no
-  git flow release finish --showcommands -p -D -m "chore(release): Release $VERSION"
+  git flow release finish --showcommands -p -D $(message_opt)
   unset GIT_MERGE_AUTOEDIT
 fi
